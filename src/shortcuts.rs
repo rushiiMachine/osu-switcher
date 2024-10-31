4d975dee28b242ec839bce2d3a1ba9c025634c68
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2024-10-30 17:02:21 -0700

    feat: new server icons

--- src/shortcuts.rs
+++ src/shortcuts.rs
@@ -6,16 +6,34 @@ use std::io::Write;
 use std::path::Path;
 use std::sync::LazyLock;
 
+/// All the icons of private servers I could find.\
+/// They were converted into icon files with ImageMagick:
+/// ```shell
+/// for file in "./*.{svg,png,jpg,webp};" do
+///     magick -background transparent "$file" -define icon:auto-resize=128,64,48,32,16 -set option:wd "%[fx:(1/1)>(w/h)?(1/1*h):w]" -set option:ht "%[fx:(1/1)>(w/h)?h:(w/(1/1))]" -gravity center -background transparent -extent "%[wd]x%[ht]" "../${file%.*}.ico";
+/// done
+/// ```
+/// Known missing icons:
+/// - `osuwtf.pw`
+/// - `nerose.click`
 static ICONS: LazyLock<HashMap<&'static str, &'static [u8]>> = LazyLock::new(|| HashMap::from([
-    ("osu.ppy.sh.ico", include_bytes!("../assets/osu.ppy.sh.ico").as_slice()),
-    ("akatsuki.pw.ico", include_bytes!("../assets/akatsuki.pw.ico").as_slice()),
-    ("kurikku.pw.ico", include_bytes!("../assets/kurikku.pw.ico").as_slice()),
-    ("ez-pp.farm.ico", include_bytes!("../assets/ez-pp.farm.ico").as_slice()),
-    ("lemres.de.ico", include_bytes!("../assets/lemres.de.ico").as_slice()),
-    ("kawata.pw.ico", include_bytes!("../assets/kawata.pw.ico").as_slice()),
-    ("gatari.pw.ico", include_bytes!("../assets/gatari.pw.ico").as_slice()),
-    ("ussr.pl.ico", include_bytes!("../assets/ussr.pl.ico").as_slice()),
-    ("ripple.moe.ico", include_bytes!("../assets/ripple.moe.ico").as_slice()),
+    ("akatsuki.gg", include_bytes!("../assets/akatsuki.gg.ico").as_slice()),
+    ("akatsuki.pw", include_bytes!("../assets/akatsuki.pw.ico").as_slice()),
+    ("ez-pp.farm", include_bytes!("../assets/ez-pp.farm.ico").as_slice()),
+    ("fuquila.net", include_bytes!("../assets/fuquila.net.ico").as_slice()),
+    ("gatari.pw", include_bytes!("../assets/gatari.pw.ico").as_slice()),
+    ("halcyon.moe", include_bytes!("../assets/halcyon.moe.ico").as_slice()),
+    ("kawata.pw", include_bytes!("../assets/kawata.pw.ico").as_slice()),
+    ("kokisu.moe", include_bytes!("../assets/kokisu.moe.ico").as_slice()),
+    ("lemres.de", include_bytes!("../assets/lemres.de.ico").as_slice()),
+    ("mamesosu.net", include_bytes!("../assets/mamesosu.net.ico").as_slice()),
+    ("osunolimits.dev", include_bytes!("../assets/osunolimits.dev.ico").as_slice()),
+    ("osuokayu.moe", include_bytes!("../assets/osuokayu.moe.ico").as_slice()),
+    ("redstar.moe", include_bytes!("../assets/redstar.moe.ico").as_slice()),
+    ("ripple.moe", include_bytes!("../assets/ripple.moe.ico").as_slice()),
+    ("scosu.net", include_bytes!("../assets/scosu.net.ico").as_slice()),
+    ("seventwentyseven.xyz", include_bytes!("../assets/seventwentyseven.xyz.ico").as_slice()),
+    ("ussr.pl", include_bytes!("../assets/ussr.pl.ico").as_slice()),
 ]));
 
 pub fn setup_icons(osu_dir: &String) {
@@ -36,7 +54,7 @@ pub fn setup_icons(osu_dir: &String) {
 
     for (icon, bytes) in ICONS.iter() {
         if files.is_empty() || !files.contains(&icon.to_string()) {
-            let path = format!("{osu_dir}/icons/{icon}");
+            let path = format!("{osu_dir}/icons/{icon}.ico");
             let path = Path::new(&path);
 
             let mut file = File::create(path)
@@ -58,10 +76,10 @@ pub fn create_shortcut(desktop_path: &String, osu_dir: &String, this_exe: &Strin
             .expect("Failed to delete old shortcut")
     }
 
-    let icon_path = if ICONS.contains_key(&*format!("{server}.ico")) {
-        format!("{osu_dir}/icons/{server}.ico")
+    let icon_path = if ICONS.contains_key(server.as_str()) {
+        format!("{osu_dir}\\icons\\{server}.ico")
     } else {
-        format!("{osu_dir}/icons/osu.ppy.sh.ico")
+        format!("{osu_dir}\\osu!.exe")
     };
 
     let mut link = ShellLink::new(this_exe)
